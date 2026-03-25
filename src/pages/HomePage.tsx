f9001ac181545904f3d9f78a5812abafeb2b88c0
alonso <alonsomiguel.wpc@gmail.com>
2026-03-24 19:57:43 -0600

    Adds useSearchParams ,hook for tabs and tabs validation

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,13 +1,13 @@
-
 import {Tabs, TabsContent, TabsList, TabsTrigger} from "@/components/ui/tabs"
 import {HeroHeader} from "@/components/heroes/HeroHeader.tsx";
 import {HeroStatsDashboard} from "@/components/heroes/HeroStatsDashboard.tsx";
 import {HeroGrid} from "@/components/heroes/HeroGrid.tsx";
-import {useEffect, useState} from "react";
+import {useEffect, useMemo, useState} from "react";
 import {CustomPagination} from "@/components/custom/CustomPagination.tsx";
 import {CustomBreadcrumbs} from "@/components/custom/CustomBreadcrumbs.tsx";
 import {getHeroesByPage} from "@/actions/GetHeroesByPage.tsx";
 import {useQuery} from "@tanstack/react-query";
+import {useSearchParams} from "react-router";
 
 type HeroTab = | "all"
     | "favorites"
@@ -24,7 +24,7 @@ export default function SuperheroApp() {
      */
     const {data} = useQuery(
         {
-            queryKey:['heroes'],
+            queryKey: ['heroes'],
             queryFn: getHeroesByPage,
             staleTime: 1000 * 10, //5 seconds
         }
@@ -37,7 +37,17 @@ export default function SuperheroApp() {
     //     getHeroesByPage().then(console.log);
     // }, []);
 
-    const [heroTab, setHeroTab] = useState<HeroTab>("all");
+    // const [heroTab, setHeroTab] = useState<HeroTab>("all");
+    const [searchParams, setSearchParams] = useSearchParams('?tab=all');
+    const selectedTab = searchParams.get("tab") ?? 'all'; //uses ?? to assign all if tab is null or undefined
+
+    //we use Memo to keep track of valid tabs, this will return all iuf the tab is invalid but it will stay the same in the url
+    //we dopn't use a type because those are erased at runtime
+    const memoTab = useMemo(() => {
+        const validTabs = ["all", "favorites", "heroes", "villains"];
+        return validTabs.includes(selectedTab) ? selectedTab : "all";
+    }, [selectedTab])
+
     return (
         <>
             <>
@@ -46,28 +56,62 @@ export default function SuperheroApp() {
                     title="Superhero Universe"
                     description="Discover, explore, and manage your favorite superheroes and villains"
                 />
-                
 
-                <CustomBreadcrumbs currentPage="Super Héroes" />
+
+                <CustomBreadcrumbs currentPage="Super Héroes"/>
 
 
                 {/* Stats Dashboard */}
-                <HeroStatsDashboard />
+                <HeroStatsDashboard/>
 
                 {/* Tabs */}
-                <Tabs value={heroTab} className="mb-8">
+                <Tabs value={memoTab} className="mb-8">
                     <TabsList className="grid w-full grid-cols-4">
-                        <TabsTrigger onClick={() => setHeroTab('all')} value="all">All Characters (16)</TabsTrigger>
-                        <TabsTrigger onClick={() => setHeroTab('favorites')} value="favorites"> Favorites
-                            (3) </TabsTrigger>
-                        <TabsTrigger onClick={() => setHeroTab('heroes')} value="heroes">Heroes (12)</TabsTrigger>
-                        <TabsTrigger onClick={() => setHeroTab('villains')} value="villains">Villains (2)</TabsTrigger>
+                        {/*<TabsTrigger onClick={() => setHeroTab('all')} value="all">All Characters (16)</TabsTrigger>*/}
+
+                        {/*USING setSearchParams('?tab=all') works but deletes other params
+                        so we used prev to keep other params and only set the tab */}
+                        <TabsTrigger
+                            value="all"
+                            onClick={() => setSearchParams((prev) => {
+                                prev.set('tab', 'all');
+                                return prev;
+                            })}>
+                            All Characters (16)
+                        </TabsTrigger>
+
+                        <TabsTrigger
+                            value="favorites"
+                            onClick={() => setSearchParams((prev) => {
+                                prev.set('tab', 'favorites');
+                                return prev;
+                            })}>
+                            Favorites (3)
+                        </TabsTrigger>
+
+                        <TabsTrigger
+                            value="favorites"
+                            onClick={() => setSearchParams((prev) => {
+                                prev.set('tab', 'heroes');
+                                return prev;
+                            })}>
+                            Heroes (12)
+                        </TabsTrigger>
+
+                        <TabsTrigger
+                            value="favorites"
+                            onClick={() => setSearchParams((prev) => {
+                                prev.set('tab', 'villains');
+                                return prev;
+                            })}>
+                            Villains (2)
+                        </TabsTrigger>
                     </TabsList>
 
                     <TabsContent value="all">
                         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8"> ALL</div>
                         {/* Character Grid */}
-                        <HeroGrid heroes={data?.heroes ? data.heroes : [] } />
+                        <HeroGrid heroes={data?.heroes ? data.heroes : []}/>
                     </TabsContent>
 
                     <TabsContent value="favorites">
@@ -85,7 +129,7 @@ export default function SuperheroApp() {
 
 
                 {/* Pagination */}
-                <CustomPagination totalPages={8} currentPage={3} />
+                <CustomPagination totalPages={8} currentPage={3}/>
             </>
         </>
     )
